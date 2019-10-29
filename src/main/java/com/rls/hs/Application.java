d0095583f3daec5db6e3541bb6c92c2569d39f2c
Tusher Islam <tusher.islam@vivacomsolutions.com>
2019-10-29 16:50:52 +0600

    Added batch saving to mysql

--- src/main/java/com/rls/hs/Application.java
+++ src/main/java/com/rls/hs/Application.java
@@ -3,11 +3,13 @@ package com.rls.hs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 	@PostConstruct
 	void started() {
--- src/main/java/com/rls/hs/consumer/RabbitMQConsumer.java
+++ src/main/java/com/rls/hs/consumer/RabbitMQConsumer.java
@@ -8,14 +8,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class RabbitMQConsumer {
     private final LocationDataRepository locationDataRepository;
+    private List<RabbitLocationData> myRabbitLocationData = new ArrayList<>();
 
     @Autowired
     public RabbitMQConsumer(LocationDataRepository theLocationDataRepository) {
@@ -27,11 +30,31 @@ public class RabbitMQConsumer {
     @RabbitListener(queues = "${application.rabbitmq.queue}")
     public void receiveMessage(@Valid @Payload List<RabbitLocationData> theRabbitLocationData) {
         try {
-            locationDataRepository.saveAll(
-                    theRabbitLocationData.stream().map(l -> new DBLocationData(l)).collect(Collectors.toList())
-            );
+            myRabbitLocationData.addAll(theRabbitLocationData);
         } catch ( Exception e) {
             logger.error(e.getMessage());
+        }
+    }
+
+    @Scheduled(fixedDelayString = "${application.mysql.write.delay:1000}")
+    private void saveToDb() {
+        if (myRabbitLocationData.size() == 0) {
+            return;
+        }
+        try {
+            logger.debug("Saving To DB: {} entries", myRabbitLocationData.size());
+            long startTime = System.currentTimeMillis();
+
+            locationDataRepository.saveAll(
+                    myRabbitLocationData.stream().map(DBLocationData::new).collect(Collectors.toList())
+            );
+            long endTime = System.currentTimeMillis();
+
+            logger.debug("Time taken to save: {}ms", (endTime - startTime));
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        } finally {
+            myRabbitLocationData.clear();
         }
     }
 }
